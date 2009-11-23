688a563462996dc132bc70ba25f6dd8f2502ea30
erikvanbrakel <e.van.brakel@gmail.com>
2009-11-24 00:32:25 +0100

    Update to add comment coloring It's a bit buggy, but more colors == good.

--- src/dotlessVS.Core/DotLessScanner.cs
+++ src/dotlessVS.Core/DotLessScanner.cs
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. */
 
+using System;
 using System.Linq;
 using LessProject.DotLessIntegration.LexerWithPegSupport;
 using Microsoft.VisualStudio.Package;
@@ -29,64 +30,93 @@ namespace LessProject.DotLessIntegration
 
         public bool ScanTokenAndProvideInfoAboutIt(TokenInfo tokenInfo, ref int state)
         {
-            var token = _lex.GetNextToken();
-            if (token == null) return false;
-            switch (token.TokenType)
-            {
-                case nLess.EnLess.LeftCurly:
-                case nLess.EnLess.RightCurly:
-                case nLess.EnLess.LeftSquare:
-                case nLess.EnLess.RightSquare:
-                case nLess.EnLess.SemiColon:
-                case nLess.EnLess.Comma:
-                    tokenInfo.Color = TokenColor.Identifier;
-                    tokenInfo.Type = TokenType.Identifier;
-                    tokenInfo.Trigger = TokenTriggers.None;
-                    tokenInfo.StartIndex = token.Start;
-                    tokenInfo.EndIndex = token.End;
-                    state = (int)States.Default;
-                    break;
-                case nLess.EnLess.Colon:
-                    state = (int) States.DefiningPropertyValue;
-                    tokenInfo.Color = TokenColor.Identifier;
-                    tokenInfo.Type = TokenType.Identifier;
-                    tokenInfo.Trigger = TokenTriggers.MethodTip;
-                    tokenInfo.StartIndex = token.Start;
-                    tokenInfo.EndIndex = token.End;
-                    break;
-                case nLess.EnLess.Class:
-                case nLess.EnLess.Id:
-                case nLess.EnLess.Variable:
-                    tokenInfo.Color = TokenColor.String;
-                    tokenInfo.Type = TokenType.String;
-                    tokenInfo.Trigger = TokenTriggers.None;
-                    tokenInfo.StartIndex = token.Start;
-                    tokenInfo.EndIndex = token.End;
-                    state = (int)States.Default;
-                    break;
-                case nLess.EnLess.Ident:
-                    if (state == (int)States.DefiningPropertyValue)
+                var token = _lex.GetNextToken();
+                if (token == null) return false;
+
+                if (state == (int)States.InComment)
+                {
+                    if(token.TokenType == nLess.EnLess.SlashStarCommentEnd)
+                        state = (int) States.Default;
+                        tokenInfo.Color = TokenColor.Comment;
+                        tokenInfo.Type = TokenType.Comment;
+                        tokenInfo.Trigger = TokenTriggers.None;
+                        tokenInfo.StartIndex = token.Start;
+                        tokenInfo.EndIndex = token.End;
+                }
+                else
+                {
+                    switch (token.TokenType)
                     {
-                        tokenInfo.Color = TokenColor.Keyword;
-                        tokenInfo.Type = TokenType.Keyword;
+                        case nLess.EnLess.DoubleSlashComment:
+                            tokenInfo.Color = TokenColor.Comment;
+                            tokenInfo.Type = TokenType.Comment;
+                            tokenInfo.Trigger = TokenTriggers.None;
+                            tokenInfo.StartIndex = token.Start;
+                            tokenInfo.EndIndex = token.End;
+                            break;
+                        case nLess.EnLess.SlashStarCommentStart:
+                            tokenInfo.Color = TokenColor.Comment;
+                            tokenInfo.Type = TokenType.Comment;
+                            tokenInfo.Trigger = TokenTriggers.None;
+                            tokenInfo.StartIndex = token.Start;
+                            tokenInfo.EndIndex = token.End;
+                            state = (int) States.InComment;
+                            break;
+                        case nLess.EnLess.LeftCurly:
+                        case nLess.EnLess.RightCurly:
+                        case nLess.EnLess.LeftSquare:
+                        case nLess.EnLess.RightSquare:
+                        case nLess.EnLess.SemiColon:
+                        case nLess.EnLess.Comma:
+                            tokenInfo.Color = TokenColor.Identifier;
+                            tokenInfo.Type = TokenType.Identifier;
+                            tokenInfo.Trigger = TokenTriggers.None;
+                            tokenInfo.StartIndex = token.Start;
+                            tokenInfo.EndIndex = token.End;
+                            state = (int) States.Default;
+                            break;
+                        case nLess.EnLess.Colon:
+                            state = (int) States.DefiningPropertyValue;
+                            tokenInfo.Color = TokenColor.Identifier;
+                            tokenInfo.Type = TokenType.Identifier;
+                            tokenInfo.Trigger = TokenTriggers.MethodTip;
+                            tokenInfo.StartIndex = token.Start;
+                            tokenInfo.EndIndex = token.End;
+                            break;
+                        case nLess.EnLess.Class:
+                        case nLess.EnLess.Id:
+                        case nLess.EnLess.Variable:
+                            tokenInfo.Color = TokenColor.String;
+                            tokenInfo.Type = TokenType.String;
+                            tokenInfo.Trigger = TokenTriggers.None;
+                            tokenInfo.StartIndex = token.Start;
+                            tokenInfo.EndIndex = token.End;
+                            state = (int) States.Default;
+                            break;
+                        case nLess.EnLess.Ident:
+                            if (state == (int) States.DefiningPropertyValue)
+                            {
+                                tokenInfo.Color = TokenColor.Keyword;
+                                tokenInfo.Type = TokenType.Keyword;
+                            }
+                            else
+                            {
+                                tokenInfo.Color = TokenColor.String;
+                                tokenInfo.Type = TokenType.String;
+                            }
+                            tokenInfo.Trigger = TokenTriggers.None;
+                            tokenInfo.StartIndex = token.Start;
+                            tokenInfo.EndIndex = token.End;
+                            break;
                     }
-                    else
-                    {
-                        tokenInfo.Color = TokenColor.String;
-                        tokenInfo.Type = TokenType.String;
-                    }
-                    tokenInfo.Trigger = TokenTriggers.None;
-                    tokenInfo.StartIndex = token.Start;
-                    tokenInfo.EndIndex = token.End;
-                    break; 
             }
-            
             return true;
         }
         enum States
         {
             Default = 1,
-            DefiningPropertyValue = 2
+            DefiningPropertyValue = 2,
+            InComment
         }
     }
 }
